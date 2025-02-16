bf6ca21d480bca37579f47d84a28660c475adac8
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-02-16 17:17:52 -0300

    fix: fix shadcn colors

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import './styles/global.css'
+import { Button } from "@/components/ui/button";
 
 export function App() {
   const [data, setData] = useState(null);
@@ -21,6 +22,7 @@ export function App() {
   return (
     <div>
       <h1>ATASK</h1>
+      <Button variant="default">Create</Button>
       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Carregando...</p>}
     </div>
   );
