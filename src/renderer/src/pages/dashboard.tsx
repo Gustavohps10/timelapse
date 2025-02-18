c5e89d31d325c9f2e5a201d851e21bb60b8ecf73
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-02-18 13:32:03 -0300

    feat: add new cards in dashboard page

--- src/renderer/src/pages/dashboard.tsx
+++ src/renderer/src/pages/dashboard.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/renderer/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/renderer/components/ui/card";
 import {
     ChartContainer,
     ChartLegend,
@@ -6,6 +8,7 @@ import {
     ChartTooltipContent,
     type ChartConfig,
   } from "@/renderer/components/ui/chart";
+import { Calendar1, CalendarDays } from "lucide-react";
   import {
     LineChart,
     CartesianGrid,
@@ -14,12 +17,11 @@ import {
   } from "recharts";
   
   const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
+    { month: "Seg", desktop: 8},
+    { month: "Ter", desktop: 4},
+    { month: "Qua", desktop: 6},
+    { month: "Qui", desktop: 7},
+    { month: "Sex", desktop: 8},
   ];
   
   const chartConfig = {
@@ -27,42 +29,92 @@ import {
       label: "Desktop",
       color: "var(--primary)",
     },
-    mobile: {
-      label: "Mobile",
-      color: "var(--foreground)",
-    },
   } satisfies ChartConfig;
   
   export function Dashboard() {
     return (
-        <ChartContainer config={chartConfig} className="h-[200px] w-full">
-        <LineChart data={chartData}>
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="month"
-            tickLine={false}
-            tickMargin={10}
-            axisLine={false}
-            tickFormatter={(value) => value.slice(0, 3)}
-          />
-          <ChartTooltip content={<ChartTooltipContent />} />
-          <ChartLegend content={<ChartLegendContent />} />
-          <Line
-            type="monotone"
-            dataKey="desktop"
-            stroke="var(--primary)"
-            strokeWidth={2}
-            strokeLinecap="round"
-          />
-          <Line
-            type="monotone"
-            dataKey="mobile"
-            stroke="var(--foreground)"
-            strokeWidth={2}
-            strokeLinecap="round"
-          />
-        </LineChart>
-      </ChartContainer>
-    );
+      <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig} className="h-[200px] w-full">
+              <LineChart data={chartData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="month"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Line
+                  type="monotone"
+                  dataKey="desktop"
+                  stroke="var(--primary)"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                />
+                <Line
+                  type="monotone"
+                  dataKey="mobile"
+                  stroke="var(--foreground)"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                />
+              </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+      <div className="flex gap-2 my-2 justify-between">
+        <Card className="flex-1">
+          <CardHeader className="pb-0 text-sm">
+            <CardTitle className="flex justify-between">
+              <span>  Horas lançadas <br />(hoje)  </span>
+              <Calendar1/>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+          <h1 className="scroll-m-20 text-2xl mt-1 font-bold tracking-tight text-zinc-800 dark:text-zinc-300 font-mono">
+            3:20
+          </h1>
+          </CardContent>
+        </Card>
+        
+        <Card  className="flex-1">
+          <CardHeader  className="pb-0 text-sm font-normal">
+            <CardTitle className="flex justify-between">
+              <span className="block">Horas lançadas  <br />(semana)</span> 
+              <CalendarDays/>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h1 className="scroll-m-20 text-2xl mt-1 font-bold tracking-tight text-zinc-800 dark:text-zinc-300 font-mono">
+            45:30
+          </h1>
+          </CardContent>
+        </Card>
+      
+        <Card  className="flex-1">
+          <CardHeader className="pb-0 text-sm">
+            <CardTitle className="flex justify-between">
+              <span className="block">Horas lançadas  <br />(mês)</span> 
+            <CalendarDays/>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h1 className="scroll-m-20 text-2xl mt-1 font-bold tracking-tight text-zinc-800 dark:text-zinc-300 font-mono">
+            113:45
+          </h1>
+          </CardContent>
+        </Card>
+      </div>
+      <Button className="cursor-pointer">Novo Apontamento</Button>
+      </>
+  );
   }
   
