48b3ae9d12a7081184c1fafd6592e172f46236b5
gustavo.henrique <gustavo.henrique@atak.com.br>
2025-02-21 13:38:08 -0300

    chore: add default filter sprint.json

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import './styles/global.css'
 import { SidebarProvider } from "@/renderer/components/ui/sidebar";
 import { ThemeProvider } from "@/renderer/components/theme-provider";
@@ -6,23 +5,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
 export function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    async function getMainData() {
-      try {
-        // Acesse a função fetchRedmine da API exposta
-        const jsonData = await window.api.fetchRedmine(); 
-        setData(jsonData);
-        console.log(data)
-      } catch (error) {
-        console.error("Erro ao buscar dados:", error);
-      }
-    }
-
-    getMainData();
-  }, []);
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <SidebarProvider>
