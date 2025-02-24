7069b5c824c8b0e704b4b59919375527e065f61b
gustavo.henrique <gustavo.henrique@atak.com.br>
2025-02-23 21:08:32 -0300

    feat: add logout

--- src/renderer/components/app-sidebar.tsx
+++ src/renderer/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { ChartLine, FileText, Search, Settings, Timer, ChevronRight } from "lucide-react";
+import { ChartLine, FileText, Search, Settings, Timer, ChevronRight, User, LogOut } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
@@ -13,43 +13,31 @@ import {
   SidebarMenuSub,
   SidebarMenuSubItem,
 } from "@/renderer/components/ui/sidebar";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible";
 import logoAtak from '../src/assets/images/icon-atak.png'
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import { useAuth } from "../hooks/use-auth";
 
 const mainItems = [
-  {
-    title: "Dashboard",
-    url: "/",
-    icon: ChartLine,
-  },
-  {
-    title: "Temporizador",
-    url: "/timer",
-    icon: Timer,
-  },
-  {
-    title: "Documentação",
-    url: "/docs",
-    icon: FileText,
-  },
+  { title: "Dashboard", url: "/", icon: ChartLine },
+  { title: "Temporizador", url: "/timer", icon: Timer },
+  { title: "Documentação", url: "/docs", icon: FileText },
 ];
 
 const subItems = [
-  {
-    title: "Buscar",
-    url: "/search",
-    icon: Search,
-  },
-  {
-    title: "Configurações",
-    url: "/settings",
-    icon: Settings,
-  },
+  { title: "Buscar", url: "/search", icon: Search },
+  { title: "Configurações", url: "/settings", icon: Settings },
 ];
 
 export function AppSidebar() {
+  const { logout } = useAuth()
+
+  const handleLogout = () => {
+    logout(); 
+  };
+
   return (
     <Sidebar collapsible="none">
       <SidebarHeader>
@@ -69,21 +57,20 @@ export function AppSidebar() {
             <SidebarMenu>
               {mainItems.map((item) => (
                 <SidebarMenuItem key={item.url}>
-                <SidebarMenuButton asChild>
-                  <NavLink 
-                    to={item.url}
-                    className="[&.active]:bg-zinc-100 dark:[&.active]:bg-zinc-800 flex items-center h-9! rounded-md transition-colors"
-                  >
-                    {({ isActive }) => (
-                      <>
-                        <item.icon className={isActive ? "text-primary" : "text-foreground"} />
-                        <span>{item.title}</span>
-                      </>
-                    )}
-                  </NavLink>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              
+                  <SidebarMenuButton asChild>
+                    <NavLink 
+                      to={item.url}
+                      className="[&.active]:bg-zinc-100 dark:[&.active]:bg-zinc-800 flex items-center h-9! rounded-md transition-colors"
+                    >
+                      {({ isActive }) => (
+                        <>
+                          <item.icon className={isActive ? "text-primary" : "text-foreground"} />
+                          <span>{item.title}</span>
+                        </>
+                      )}
+                    </NavLink>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
@@ -121,7 +108,16 @@ export function AppSidebar() {
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
-        Rodapé
+        <div className="hover:bg-zinc-100 dark:hover:bg-zinc-800 p-2 rounded-md flex gap-2 items-center">
+          <User className="text-zinc-900 bg-zinc-200 rounded-lg h-8 w-8 p-2"/>
+          <div className="flex flex-col">
+            <h1 className="scroll-m-20 text-sm font-bold tracking-tighter">username.aleatorio</h1>
+            <h2 className="text-muted-foreground scroll-m-20 text-sm font tracking-tight leading-none">Developer</h2>
+          </div>
+          <Button size="icon" variant="outline" className="ml-auto h-8 w-8" onClick={handleLogout}>
+            <LogOut />
+          </Button>
+        </div>
       </SidebarFooter>
     </Sidebar>
   );
