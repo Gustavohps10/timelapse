74bb8b8a98ddf19ab820f5f0ed800d4a46492a16
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-02-27 13:19:40 -0300

    chore: configure react-markdown with Tailwind V4

--- src/renderer/src/pages/docs.tsx
+++ src/renderer/src/pages/docs.tsx
@@ -1,7 +1,15 @@
+import ReactMarkdown from "react-markdown";
+
 export function Docs() {
-    return (
-        <>
-        DOCUMENTACOES RENDERIZADAS
-        </>
-    )
+  const markdownContent = `> Hello world!`;
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Teste de Tela de Documento</h1>
+      <div className="prose">
+  <ReactMarkdown>{markdownContent}</ReactMarkdown>
+</div>
+
+    </div>
+  );
 }
