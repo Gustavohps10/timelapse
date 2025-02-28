ec958439736d68ed0ff039e6e4003a22dec1238f
gustavo.henrique <gustavo.henrique@atak.com.br>
2025-02-28 13:35:07 -0300

    feat: start editor features with tiptap

--- src/renderer/src/pages/docs.tsx
+++ src/renderer/src/pages/docs.tsx
@@ -1,15 +1,40 @@
-import ReactMarkdown from "react-markdown";
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
+const initialContent = `
+        # Título Nível 1
+        ## Título Nível 2
+        ### Título Nível 3
+
+        > Este é um bloco de citação.
+
+        **Texto em negrito**
+
+        *Texto em itálico*
+
+        [Link Exemplo](https://exemplo.com)
+
+        - Item de lista não ordenada
+        1. Item de lista ordenada
+      `
 
 export function Docs() {
-  const markdownContent = `> Hello world!`;
+
+  // Inicializa o editor com o conteúdo obtido
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: initialContent,
+  });
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">Teste de Tela de Documento</h1>
-      <div className="prose">
-  <ReactMarkdown>{markdownContent}</ReactMarkdown>
-</div>
-
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div
+        className="prose lg:prose-xl bg-white shadow-md p-16 m-20 border"
+        style={{ width: '793.7px', height: '1122.5px' }}
+      >
+        {/* Renderiza o conteúdo do editor */}
+        <EditorContent editor={editor} />
+      </div>
     </div>
   );
 }
