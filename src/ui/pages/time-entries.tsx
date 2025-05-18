2fe7aaf2151a1d353d08d6133752e91491317917
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-05-17 23:30:23 -0300

    feat: add time entries page layout structure

--- src/ui/pages/time-entries.tsx
+++ src/ui/pages/time-entries.tsx
@@ -1,13 +1,41 @@
 import { format } from 'date-fns'
-import { Shapes, Ticket } from 'lucide-react'
+import {
+  BarChart2,
+  Briefcase,
+  CalendarCheck,
+  CalendarDaysIcon,
+  CheckCircle,
+  ClipboardCheck,
+  Code,
+  FileText,
+  FlaskConical,
+  GraduationCap,
+  Handshake,
+  Hash,
+  LifeBuoy,
+  Palette,
+  Play,
+  SearchCode,
+  Settings,
+  Users,
+  Wrench,
+} from 'lucide-react'
 import { useState } from 'react'
 
+import { columns } from '@/ui/components/time-entries-table/columns'
+import {
+  DataTable,
+  TimeEntry,
+} from '@/ui/components/time-entries-table/data-table'
 import { Button } from '@/ui/components/ui/button'
 import { Calendar } from '@/ui/components/ui/calendar'
 import { Card } from '@/ui/components/ui/card'
-import { Checkbox } from '@/ui/components/ui/checkbox'
 import { Input } from '@/ui/components/ui/input'
-import { Label } from '@/ui/components/ui/label'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/ui/components/ui/popover'
 import {
   Select,
   SelectContent,
@@ -16,77 +44,108 @@ import {
   SelectValue,
 } from '@/ui/components/ui/select'
 
+const items = [
+  { value: '-', label: '--- Selecione ---', icon: null },
+  { value: '8', label: 'Design', icon: Palette },
+  { value: '9', label: 'Desenvolvimento', icon: Code },
+  { value: '10', label: 'Análise', icon: BarChart2 },
+  { value: '11', label: 'Planejamento', icon: CalendarCheck },
+  { value: '12', label: 'Encerramento', icon: CheckCircle },
+  { value: '13', label: 'Teste', icon: FlaskConical },
+  { value: '14', label: 'Revisão Código', icon: SearchCode },
+  { value: '15', label: 'Gerência de Configuração', icon: Settings },
+  { value: '16', label: 'Correção', icon: Wrench },
+  { value: '17', label: 'Suporte', icon: LifeBuoy },
+  { value: '18', label: 'Apoio', icon: Handshake },
+  { value: '19', label: 'Homologação', icon: ClipboardCheck },
+  { value: '25', label: 'Documentação', icon: FileText },
+  { value: '26', label: 'Treinamento', icon: GraduationCap },
+  { value: '27', label: 'Reunião', icon: Users },
+  { value: '28', label: 'Gestão', icon: Briefcase },
+]
+
+export const timeEntries: TimeEntry[] = Array.from({ length: 10 }, (_, i) => ({
+  project_id: 1,
+  user_id: 1,
+  issue_id: 567850 + i,
+  hours: Math.floor(Math.random() * 8) + 1,
+  comments: `Entrada de tempo ${i + 1}`,
+  spent_on: new Date(Date.now() - i * 86400000).toISOString().split('T')[0],
+}))
+
 export function TimeEntries() {
   const [date, setDate] = useState<Date | undefined>(new Date())
 
   return (
     <Card className="space-y-4 p-4">
       <div className="flex items-center gap-2">
-        <div>
-          <Label className="text-muted-foreground flex items-center gap-1 text-sm">
-            <Ticket size={20} /> Ticket
-          </Label>
-          <Input placeholder="Ticket" className="max-w-[150px]" />
+        <div className="flex items-stretch">
+          <div className="border-input flex w-10 items-center justify-center rounded-l-md border">
+            <Hash size={16} />
+          </div>
+          <Input placeholder="Ticket" className="rounded-l-none" />
         </div>
 
-        <div>
-          <Label className="text-muted-foreground flex items-center gap-1 text-sm">
-            <Shapes size={20} /> Atividade
-          </Label>
-
-          <Select>
-            <SelectTrigger className="w-[200px]">
-              <SelectValue placeholder="Atividade" />
-            </SelectTrigger>
+        <Select>
+          <SelectTrigger className="w-3xs cursor-pointer">
+            <SelectValue placeholder="Atividade" />
+          </SelectTrigger>
+          <SelectContent>
             <SelectContent>
-              <SelectItem value="-">--- Selecione ---</SelectItem>
-              <SelectItem value="8">Design</SelectItem>
-              <SelectItem value="9">Desenvolvimento</SelectItem>
-              <SelectItem value="10">Analise</SelectItem>
-              <SelectItem value="11">Planejamento</SelectItem>
-              <SelectItem value="12">Encerramento</SelectItem>
-              <SelectItem value="13">Teste</SelectItem>
-              <SelectItem value="14">Revisão Código</SelectItem>
-              <SelectItem value="15">Gerência de Configuração</SelectItem>
-              <SelectItem value="16">Correção</SelectItem>
-              <SelectItem value="17">Suporte</SelectItem>
-              <SelectItem value="18">Apoio</SelectItem>
-              <SelectItem value="19">Homologação</SelectItem>
-              <SelectItem value="25">Documentação</SelectItem>
-              <SelectItem value="26">Treinamento</SelectItem>
-              <SelectItem value="27">Reunião</SelectItem>
-              <SelectItem value="28">Gestão</SelectItem>
+              {items.map(({ value, label, icon: IconComponent }) => (
+                <SelectItem
+                  key={value}
+                  value={value}
+                  className="cursor-pointer"
+                >
+                  {!!IconComponent && <IconComponent size={16} />}
+                  {label}
+                </SelectItem>
+              ))}
             </SelectContent>
-          </Select>
+          </SelectContent>
+        </Select>
+
+        <Button className="rounded-xl">
+          <Play />
+        </Button>
+
+        <div className="flex w-40 items-center justify-center rounded-md border p-2">
+          <span className="mt-1 scroll-m-20 font-mono text-4xl leading-tight font-bold tracking-tighter text-zinc-800 dark:text-zinc-300">
+            00:00
+          </span>
+        </div>
+      </div>
+
+      <div className="py-4">
+        <div className="flex items-center gap-2">
+          <Button variant="outline">Anterior</Button>
+          <Button variant="outline">Hoje</Button>
+          <Button variant="outline">Próximo</Button>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline" className="min-w-56">
+                <CalendarDaysIcon />
+                {date
+                  ? format(date, 'EEEE - dd/MM/yyyy')
+                  : 'Selecione uma data'}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-1">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                className="m-none"
+              />
+            </PopoverContent>
+          </Popover>
         </div>
 
-        <Button>Iniciar</Button>
-      </div>
-
-      <div className="flex items-center gap-2">
-        <Button variant="outline">Anterior</Button>
-        <Button variant="outline">Hoje</Button>
-        <Button variant="outline">Próximo</Button>
-        <span className="mx-2 font-semibold">
-          {date ? format(date, 'EEEE - dd/MM/yyyy') : 'Selecione uma data'}
-        </span>
-        <Checkbox className="ml-auto" id="exibir" />
-        <label htmlFor="exibir" className="text-sm">
-          Exibir Comentários
-        </label>
-      </div>
-
-      <Card className="block">
-        <Calendar mode="single" selected={date} onSelect={setDate} />
-      </Card>
-
-      <div className="mt-4 grid grid-cols-6 gap-2 border-t pt-2 text-sm font-medium">
-        <div>Descrição</div>
-        <div>Hr. Ini.</div>
-        <div>Hr. Fin.</div>
-        <div>Dif.</div>
-        <div>Ticket</div>
-        <div>Atividade</div>
+        <div className="container mx-auto py-4">
+          <DataTable columns={columns} data={timeEntries} />
+        </div>
       </div>
     </Card>
   )
