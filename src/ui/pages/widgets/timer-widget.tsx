6e2321087eb7f5dde36950804965941a32741ce1
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-05-23 00:26:11 -0300

    feat: add toggle form section for timer widget

--- src/ui/pages/widgets/timer-widget.tsx
+++ src/ui/pages/widgets/timer-widget.tsx
@@ -8,11 +8,13 @@ import {
   ClipboardCheck,
   ClockArrowUp,
   Code,
+  EllipsisVertical,
   FileText,
   FlaskConical,
   GraduationCap,
-  GripVertical,
   Handshake,
+  Hash,
+  LetterText,
   LifeBuoy,
   Palette,
   Pin,
@@ -22,7 +24,7 @@ import {
   Users,
   Wrench,
 } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { Timer } from '@/ui/components/timer'
 import { Badge } from '@/ui/components/ui/badge'
@@ -41,6 +43,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/ui/components/ui/popover'
+import { ScrollArea } from '@/ui/components/ui/scroll-area'
 import { cn } from '@/ui/lib/utils'
 
 const activities = [
@@ -61,13 +64,13 @@ const activities = [
   { value: '27', label: 'Reunião', icon: Users },
   { value: '28', label: 'Gestão', icon: Briefcase },
 ]
+
 export function TimerWidget() {
   const [open, setOpen] = useState(false)
   const [value, setValue] = useState('')
 
   useEffect(() => {
     document.body.style.background = 'transparent'
-
     return () => {
       document.body.style.background = ''
     }
@@ -76,7 +79,7 @@ export function TimerWidget() {
   return (
     <div className="h-screen w-screen overflow-hidden px-10 py-4 text-white">
       <div
-        className="drag-bar relative my-2 flex justify-center rounded-md p-2"
+        className="drag-bar relative my-2 flex justify-center rounded-md border p-2"
         style={
           {
             backgroundColor: 'rgba(0,0,0,0.4)',
@@ -90,12 +93,13 @@ export function TimerWidget() {
       >
         <Badge
           variant="outline"
-          className="absolute top-0 left-0 -translate-[50%] bg-[#0000007c]"
+          className="absolute top-0 left-0 -translate-y-[120%] bg-[rgba(0,0,0,0.4)]"
         >
           #51094
         </Badge>
         <Timer size="medium" />
       </div>
+
       <div className="flex items-center justify-between gap-1">
         <div className="flex items-center gap-1">
           <Button className="h-7 w-7">
@@ -115,60 +119,127 @@ export function TimerWidget() {
           </Button>
         </div>
 
-        <Button size="sm" className="w-2 bg-[#00000031] hover:bg-[#000000a8]">
-          <GripVertical className="w-3" />
+        <Button
+          size="sm"
+          className="h-8 w-8 bg-[#00000031] p-2 hover:bg-[#000000a8]"
+          onClick={() => setOpen(!open)}
+          aria-expanded={open}
+          aria-label="Toggle sessão expansiva"
+        >
+          <EllipsisVertical className="h-4 w-4" />
         </Button>
       </div>
-      <div className="my-2 w-full">
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              role="combobox"
-              aria-expanded={open}
-              className="bg-background w-full justify-between hover:bg-neutral-900"
-            >
-              {value
-                ? activities.find((framework) => framework.value === value)
-                    ?.label
-                : 'Atividade'}
-              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-full p-0">
-            <Command>
-              <CommandInput placeholder="Search framework..." />
-              <CommandList>
-                <CommandEmpty>No framework found.</CommandEmpty>
-                <CommandGroup>
-                  {activities.map((framework) => (
-                    <CommandItem
-                      key={framework.value}
-                      value={framework.value}
-                      onSelect={(currentValue) => {
-                        setValue(currentValue === value ? '' : currentValue)
-                        setOpen(false)
-                      }}
-                    >
-                      <Check
-                        className={cn(
-                          'mr-2 w-full',
-                          value === framework.value
-                            ? 'opacity-100'
-                            : 'opacity-0',
+
+      {open && (
+        <ExpansiveTimeWigetSession
+          value={value}
+          setValue={setValue}
+          setOpen={setOpen}
+        />
+      )}
+    </div>
+  )
+}
+
+interface ExpansiveTimeWigetSessionProps {
+  value: string
+  setValue: (val: string) => void
+  setOpen: (open: boolean) => void
+}
+
+function ExpansiveTimeWigetSession({
+  value,
+  setValue,
+}: ExpansiveTimeWigetSessionProps) {
+  const [commandInput, setCommandInput] = useState('')
+  const [popoverOpen, setPopoverOpen] = useState(false)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    inputRef.current?.blur()
+  }, [])
+
+  return (
+    <div className="my-2 w-full [&_*]:text-[12px]">
+      <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={true}
+            className="bg-background h-8 w-full justify-between hover:bg-neutral-900"
+          >
+            {value
+              ? activities.find((framework) => framework.value === value)?.label
+              : 'Atividade'}
+            <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0">
+          <Command className="w-[140px]">
+            <CommandInput
+              ref={inputRef}
+              placeholder="Procurar"
+              className="h-8"
+              style={{ height: 32 }}
+              value={commandInput}
+              onValueChange={setCommandInput}
+            />
+            <CommandList>
+              <CommandEmpty>Nenhuma atividade encontrada.</CommandEmpty>
+              <CommandGroup>
+                <ScrollArea className="h-[192px] rounded-md [&_*]:text-[12px]">
+                  {activities
+                    .filter((framework) =>
+                      framework.label
+                        .toLowerCase()
+                        .includes(commandInput.toLowerCase()),
+                    )
+                    .map((framework) => (
+                      <CommandItem
+                        key={framework.value}
+                        value={framework.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? '' : currentValue)
+                          setPopoverOpen(false)
+                        }}
+                        className="cursor-pointer [&_*]:text-[10px]"
+                      >
+                        <Check
+                          className={cn(
+                            'mr-2 w-full',
+                            value === framework.value
+                              ? 'opacity-100'
+                              : 'opacity-0',
+                          )}
+                        />
+                        {framework.icon && (
+                          <framework.icon style={{ width: 14 }} />
                         )}
-                      />
-                      {/* {framework.icon && <framework.icon />} */}
-                      {framework.label}
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
-        <Input className="bg-background my-1" placeholder="Ticket" />
-        <Input className="bg-background" placeholder="Descricao" />
+                        {framework.label}
+                      </CommandItem>
+                    ))}
+                </ScrollArea>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+
+      <div className="relative my-1">
+        <Hash
+          className="text-muted-foreground pointer-events-none absolute top-1/2 left-2 h-4 w-4 -translate-y-1/2"
+          style={{ width: 14 }}
+        />
+        <Input placeholder="Ticket" className="bg-background h-8 pl-7" />
+      </div>
+
+      <div className="relative my-1">
+        <LetterText
+          className="text-muted-foreground pointer-events-none absolute top-1/2 left-2 h-4 w-4 -translate-y-1/2"
+          style={{ width: 14 }}
+        />
+        <Input placeholder="Descricao" className="bg-background h-8 pl-7" />
       </div>
     </div>
   )
