229934603dd53849dbf8eb5cabb4b260ed3e6734
gustavo.henrique <gustavo.henrique@atak.com.br>
2025-06-11 23:33:04 -0300

    refactor: remove sidebar from login page and add footer to login layout

--- src/ui/pages/layouts/auth-layout.tsx
+++ src/ui/pages/layouts/auth-layout.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from 'react-router-dom'
 
-import { AppSidebar } from '@/ui/components/app-sidebar'
+import { Footer } from '@/ui/components/footer'
 import { ScrollArea } from '@/ui/components/ui/scroll-area'
 
 export function AuthLayout() {
   return (
     <>
-      <AppSidebar />
       <main className="h-[100vh] flex-1 overflow-hidden">
         <ScrollArea className="h-[100vh] p-4">
           <Outlet />
+          <Footer />
         </ScrollArea>
       </main>
     </>
