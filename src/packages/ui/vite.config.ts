7ec884b59862911f600071b062b90cbbe6a4fbc7
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-07-07 22:57:18 -0300

    fix(styles): export compiled globals.css from UI package

--- src/packages/ui/vite.config.ts
+++ src/packages/ui/vite.config.ts
@@ -3,43 +3,30 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react(),
+    tailwindcss(),
     dts({
       rollupTypes: true,
     }),
     tsconfigPaths(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/styles/*.css',
-          dest: 'styles',
-        },
-        {
-          src: 'src/assets/*',
-          dest: 'assets',
-        },
-      ],
-    }),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
     },
-    assetsInlineLimit: 0,
+    cssCodeSplit: true,
     rollupOptions: {
       external: ['react', 'react-dom', 'react-router-dom'],
       output: {
         preserveModules: true,
         preserveModulesRoot: 'src',
         entryFileNames: '[name].js',
-        assetFileNames: 'assets/[name][extname]',
+        assetFileNames: '[name][extname]',
       },
     },
   },
