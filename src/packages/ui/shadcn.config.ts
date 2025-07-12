b90b0726beca05bd03caeead0623678db3479880
gustavo.henrique <gustavo.henrique@atak.com.br>
2025-07-12 19:41:49 -0300

    feat(ui): configure shadcn/ui for monorepo support and fix components path

---
+++ src/packages/ui/shadcn.config.ts
@@ -0,0 +1,7 @@
+const config = {
+  outdir: './src/components', // <- caminho relativo ao pacote UI
+  tsconfig: './tsconfig.json', // tsconfig local do UI
+  alias: '@/',
+}
+
+export default config
