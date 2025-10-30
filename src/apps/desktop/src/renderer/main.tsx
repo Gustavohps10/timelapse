aaf47141fb83e22d2f6a236e928a577c0567d11b
Gustavohps10 <gustavoh.santos735@gmail.com>
2025-10-30 00:00:54 -0300

    chore: add react dev tools

--- src/apps/desktop/src/renderer/main.tsx
+++ src/apps/desktop/src/renderer/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 
 import { AppDesktop } from '@/renderer/App'
 
+console.log(import.meta.env.MODE)
+
+// if (import.meta.env.MODE === 'development') {
+//   // @ts-ignore
+//   import('@welldone-software/why-did-you-render').then(
+//     ({ default: whyDidYouRender }) => {
+//       whyDidYouRender(React, { trackAllPureComponents: true })
+//     },
+//   )
+// }
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AppDesktop />
